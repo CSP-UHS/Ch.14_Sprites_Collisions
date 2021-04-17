fcdee8b846f65030f7f93a908afb33c20621fa01
mitchelldan <mitchelldan@urbandaleschools.com>
2021-04-17 01:41:18 -0500

    did thing

--- 15.0_Jedi_Training.py
+++ 15.0_Jedi_Training.py
@@ -5,6 +5,7 @@
 
 import random
 import arcade
+import random
 
 # --- Constants ---
 BB8_scale = 0.3
@@ -12,24 +13,123 @@ trooper_scale = 0.1
 trooper_count = 40
 SW = 800
 SH = 600
+SP = 4
+
+
+class Player(arcade.Sprite):
+    def __init__(self):
+        super().__init__("Images/bb8.png", BB8_scale)
+        self.laser = arcade.load_sound("sounds/laser.mp3")
+
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        if self.left < 0:
+            self.left = 0
+            arcade.play_sound(self.laser)
+        elif self.right > SW:
+            self.right = SW
+            arcade.play_sound(self.laser)
+        if self.bottom < 0:
+            self.bottom = 0
+            arcade.play_sound(self.laser)
+        elif self.top > SH:
+            self.top = SH
+            arcade.play_sound(self.laser)
+
+
+
+
+class Trooper(arcade.Sprite):
+    def __init__(self):
+        super().__init__("Images/stormtrooper.png", trooper_scale)
+        self.w = int(self.width)
+        self.h = int(self.height)
+
+    def update(self):
+        pass
 
 #------MyGame Class--------------
 class MyGame(arcade.Window):
 
     def __init__(self,SW,SH,title):
         super().__init__(SW, SH, title)
+        self.set_mouse_visible(False)
         arcade.set_background_color(arcade.color.SKY_BLUE)
+
+    def reset(self):
+
+        self.player_list = arcade.SpriteList()
+        self.trooper_list = arcade.SpriteList()
+
+        self.score = 0
+
+        self.BB8 = Player()
+        self.BB8.center_x = SW / 2
+        self.BB8.center_y = SH / 2
+        self.player_list.append(self.BB8)
+
+        for i in range(trooper_count):
+            trooper = Trooper()
+            trooper.center_x = random.randrange(trooper.w,SW-trooper.w)
+            trooper.center_y = random.randrange(trooper.h,SH-trooper.h)
+            self.trooper_list.append(trooper)
+
 
     def on_draw(self):
         arcade.start_render()
+        self.trooper_list.draw()
+        self.player_list.draw()
+
+        output = f"Score: {self.score}"
+        arcade.draw_text(output,10,20, arcade.color.BLACK, 14)
 
     def on_update(self, dt):
-        pass
+        self.player_list.update()
+        self.trooper_list.update()
+
+        trooper_hit_list = arcade.check_for_collision_with_list(self.BB8,self.trooper_list)
+        for troop in trooper_hit_list:
+            troop.kill()
+            self.score +=1
+            arcade.play_sound(self.BB8.laser)
+
+    def on_key_press(self, symbol, modifiers: int):
+        print(symbol)
+        if symbol ==119:
+            self.BB8.change_y = SP
+
+        if symbol ==115:
+            self.BB8.change_y = -SP
+
+        if symbol ==97:
+            self.BB8.change_x = -SP
+
+        if symbol ==100:
+            self.BB8.change_x = SP
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        print(symbol)
+        if symbol ==119:
+            self.BB8.change_y = 0
+
+        if symbol ==115:
+            self.BB8.change_y = 0
+
+        if symbol ==97:
+            self.BB8.change_x = 0
+
+        if symbol ==100:
+            self.BB8.change_x = 0
+
+
 
 
 #-----Main Function--------
 def main():
     window = MyGame(SW,SH,"BB8 Attack")
+    window.reset()
     arcade.run()
 
 #------Run Main Function-----
--- 15.1_Game.py
+++ 15.1_Game.py
@@ -19,26 +19,251 @@ import random
 import arcade
 
 # --- Constants ---
+BB8_scale = 0.6
+trooper_scale = 0.1
+trooper_count = 40
 SW = 800
 SH = 600
+SP = 4
+
+
+class Player(arcade.Sprite):
+    def __init__(self):
+        super().__init__("Images/bb8.png", BB8_scale)
+        self.laser = arcade.load_sound("game_assets/Sounds/coin.wav")
+        self.grounded = False
+        self.slowing = False
+
+    def slowdown(self):
+        if self.change_x > 0: #or self.change_x < 0:
+            self.change_x = self.change_x * 0.9
+            if self.change_x < 0.5:
+                self.change_x = 0
+                self.slowing = False
+        if self.change_x < 0: #or self.change_x < 0:
+            self.change_x = self.change_x * 0.9
+            if self.change_x > -0.5:
+                self.change_x = 0
+                self.slowing = False
+
+    def update(self):
+        self.change_y -= (8/60)
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        if self.left < 0:
+            self.left = 0
+            #arcade.play_sound(self.laser)
+        elif self.right > SW:
+            self.right = SW
+            #arcade.play_sound(self.laser)
+        if self.bottom < 80:
+            self.bottom = 80
+            self.grounded = True
+            #arcade.play_sound(self.laser)
+        elif self.top > SH:
+            self.top = SH
+            #arcade.play_sound(self.laser)
+
+        if self.slowing == True:
+            if self.grounded == True:
+                self.slowdown()
+
+
+
+
+class Trooper(arcade.Sprite):
+    def __init__(self):
+        self.textureframe = 0
+        super().__init__()
+        self.w = int(self.width)
+        self.h = int(self.height)
+        self.angle = 0
+        self.scale = 0.2
+
+        #self.draw_hit_box()
+
+        #self.textureframe = 0
+        self.texture_list = []
+        texture = arcade.load_texture("game_assets/Coins/frame_0.gif")
+        self.texture_list.append(texture)
+        texture = arcade.load_texture("game_assets/Coins/frame_1.gif")
+        self.texture_list.append(texture)
+        texture = arcade.load_texture("game_assets/Coins/frame_2.gif")
+        self.texture_list.append(texture)
+        texture = arcade.load_texture("game_assets/Coins/frame_3.gif")
+        self.texture_list.append(texture)
+        texture = arcade.load_texture("game_assets/Coins/frame_4.gif")
+        self.texture_list.append(texture)
+        texture = arcade.load_texture("game_assets/Coins/frame_5.gif")
+        self.texture_list.append(texture)
+        self.texture = texture
+
+
+    def update(self):
+        #self.angle += 1
+        pass
+
+    def update_animation(self, delta_time: float = 1/15):
+        #print("test")
+        self.textureframe += 1
+        if self.textureframe > 5:
+            self.textureframe = 0
+        self.texture = self.texture_list[self.textureframe]
+
+class Backround():
+    def __init__(self, speed,x,y,h):
+        self.speed = speed
+        self.x = x
+        self.y = y
+        self.h = h
+
+    def drawmount(self):
+        if self.x > -200 and self.x < 1000:
+            arcade.draw_rectangle_filled(self.x,self.y,self.h,self.h,arcade.color.GREEN,45)
+            arcade.draw_rectangle_outline(self.x, self.y, self.h, self.h, arcade.color.GRAY,1,45)
+
+    def updatemont(self):
+        self.x += self.speed
+        if self.x < -200:
+            self.x = random.randint(1000,1200)
+
+class World(arcade.Sprite):
+    def __init__(self,texpath):
+        self.texturepath = texpath
+        super().__init__(self.texturepath, 1)
+        self.laser = arcade.load_sound("game_assets/Sounds/coin.wav")
+
+    def update(self):
+        pass
+
 
 #------MyGame Class--------------
 class MyGame(arcade.Window):
 
     def __init__(self,SW,SH,title):
+        self.time = 0
         super().__init__(SW, SH, title)
-        arcade.set_background_color(arcade.color.WHITE)
+        self.set_mouse_visible(False)
+        self.set_vsync(True)
+        arcade.set_background_color(arcade.color.SKY_BLUE)
+
+    def reset(self):
+
+
+        #this is the list of the grass blocks
+        self.worldlist = arcade.SpriteList()
+        for i in range(int(SW / 32)):
+            self.floor = World("game_assets/worldtiles/grass.png")
+            self.floor.center_x = (32 / 2) + (32 * i)
+            self.floor.center_y = 80 -16
+            self.worldlist.append(self.floor)
+
+        for i in range(int(SW / 32)):
+            self.floor = World("game_assets/worldtiles/Dirt1.png")
+            self.floor.center_x = (32 / 2) + (32 * i)
+            self.floor.center_y = 80 -16 -32
+            self.worldlist.append(self.floor)
+
+        self.player_list = arcade.SpriteList()
+        self.trooper_list = arcade.SpriteList()
+
+        self.backlist = []
+        for i in range(25):
+            background = Backround(-0.5, random.randint(-200, 1000), 80, random.randint(50, 200))
+            self.backlist.append(background)
+
+        self.score = 0
+
+        self.BB8 = Player()
+        self.BB8.center_x = SW / 2
+        self.BB8.center_y = SH / 2
+        self.player_list.append(self.BB8)
+
+        for i in range(trooper_count):
+            trooper = Trooper()
+            trooper.center_x = random.randrange(trooper.w,SW-trooper.w)
+            trooper.center_y = random.randrange(trooper.h+90,300)
+            self.trooper_list.append(trooper)
+
 
     def on_draw(self):
         arcade.start_render()
 
+        for obj in self.backlist:
+            obj.drawmount()
+
+        self.worldlist.draw()
+
+        self.trooper_list.draw()
+        self.player_list.draw()
+
+        arcade.draw_rectangle_filled(SW/2,5,SW,30,arcade.color.BLACK)
+        output = f"Score: {self.score}"
+        arcade.draw_text(output,10,1, arcade.color.WHITE, 14)
+
     def on_update(self, dt):
-        pass
+        self.time +=1
+        if self.time == 4:
+            self.time = 0
+            self.trooper_list.update_animation()
+        self.player_list.update()
+        self.trooper_list.update()
+
+        for obj in self.backlist:
+            obj.updatemont()
+
+        trooper_hit_list = arcade.check_for_collision_with_list(self.BB8,self.trooper_list)
+        for troop in trooper_hit_list:
+            troop.kill()
+            self.score +=1
+            arcade.play_sound(self.BB8.laser)
+
+
+    def on_key_press(self, symbol, modifiers: int):
+        print(symbol)
+        # if symbol ==119:
+        #     self.BB8.change_y = SP
+        #
+        # if symbol ==115:
+        #     self.BB8.change_y = -SP
+
+        if symbol ==97:
+            self.BB8.slowing = False
+            self.BB8.change_x = -SP
+
+        if symbol ==100:
+            self.BB8.slowing = False
+            self.BB8.change_x = SP
+
+        if symbol == 32:
+            if self.BB8.grounded == True:
+                self.BB8.change_y = 6
+                self.BB8.grounded = False
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        print(symbol)
+        # if symbol ==119:
+        #     self.BB8.change_y = 0
+        #
+        # if symbol ==115:
+        #     self.BB8.change_y = 0
+
+        if symbol ==97:
+            self.BB8.slowing = True
+
+        if symbol ==100:
+            self.BB8.slowing = True
+
+
+
+
 
 
 #-----Main Function--------
 def main():
-    window = MyGame(SW,SH,"My Game")
+    window = MyGame(SW,SH,"BB8 Attack")
+    window.reset()
     arcade.run()
 
 #------Run Main Function-----
